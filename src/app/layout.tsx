b79b78eccb8694950397d7e7da5591358188f152
Farrukh Ahmed <farrukhahmed368@gmail.com>
2023-12-07 19:33:45 +0500

    Updated Layout & TRPC

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Navbar from "@/components/Navbar";
+import Providers from "@/components/Providers";
 import { cn, constructMetadata } from "@/lib/utils";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -14,11 +16,15 @@ export default function RootLayout({
   return (
     <html lang="en" className="h-full">
       <body
-        className={cn("relative h-full font-sans antialiased", inter.className)}>
-          <main className="relative flex flex-col min-h-screen">
-          <div className='flex-grow flex-1'>{children}</div>
-          </main>
-        </body>
+        className={cn("relative h-full font-sans antialiased", inter.className)}
+      >
+        <main className="relative flex flex-col min-h-screen">
+          <Providers>
+            <Navbar />
+            <div className="flex-grow flex-1">{children}</div>
+          </Providers>
+        </main>
+      </body>
     </html>
   );
 }
